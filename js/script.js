7a9e73e015ecf08f262492dd136ad83c93c2a3cf
albertliangcode <albertliangcode@gmail.com>
2015-02-21 11:51:06 -0800

    Fixing class declarations

--- js/script.js
+++ js/script.js
@@ -1,25 +1,31 @@
 $(document).ready(function()
 {
-	alert("js working");
+	console.log("js working");
 
 	$('#test').click(function(){
-		alert("test working");
-		var Classroom = Parse.Object;
+		console.log("test working");
+		var Classroom = Parse.Object.extend("Classroom");
 		var room = new Classroom();
  
-		var Post = Parse.Object.extend("Classroom");
-		room.set("upvotes", 0);
-		room.set("downvotes", 0);
-		room.set("answered", false); 
+ 		console.log("Classroom declared");
+		// var Post = Parse.Object.Classroom.extend("Post");
+		// var post = new Post();
+		// post.set("upvotes", 0);
+		// post.set("downvotes", 0);
+		// post.set("answered", false); 
+
 		room.save(null, {
+			console.log("Entered save function");
 	  		success: function(Classroom) {
 	    		// Execute any logic that should take place after the object is saved.
 	    		alert('New object created with objectId: ' + room.id);
+	    		console.log("Win");
 	  		},
 	  		error: function(Classroom, error) {
 	    		// Execute any logic that should take place if the save fails.
 	    		// error is a Parse.Error with an error code and message.
 	    		alert('Failed to create new object, with error code: ' + error.message);
+	  			console.log("Nope.");
 	  		}
 		});
 
