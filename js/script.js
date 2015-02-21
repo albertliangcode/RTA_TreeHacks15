5a0b25a358827fcebdf358a15e5ef6bfb1e3f93b
xinlei <wang.xinlei15@gmail.com>
2015-02-21 11:38:47 -0800

    button testing

--- js/script.js
+++ js/script.js
@@ -1,33 +1,35 @@
-$('#test').click(function(){
-	var Classroom = Parse.Object;
-	var room = new Classroom();
-	room.set("name","");
-	room.set("numStudents",0);
-	room.set("inSession",false);
- 
-	var Post = Parse.Object.extend("Classroom");
-	var post = new Post();
-	post.set("upvotes", 0);
-	post.set("downvotes", 0);
-	post.set("answered", false);
- 
-	room.save(null, {
-	  success: function(gameScore) {
-	    // Execute any logic that should take place after the object is saved.
-	    alert('New object created with objectId: ' + room.id);
-	  },
-	  error: function(gameScore, error) {
-	    // Execute any logic that should take place if the save fails.
-	    // error is a Parse.Error with an error code and message.
-	    alert('Failed to create new object, with error code: ' + error.message);
-	  }
-	});
+$(document).ready(function()
+{
+	alert("js working");
 
-	var TestObject = Parse.Object.extend("TestObject");
-	var testObject = new TestObject();
-	testObject.save({foo: "bar"}).then(function(object) {
-	  alert("yay! it worked");
+	$('#test').click(function(){
+		alert("test working");
+		var Classroom = Parse.Object.extend("Classroom");
+		var room = new Classroom();
+ 
+		var Post = Parse.Object.extend
+		room.set("upvotes", 0);
+		room.set("downvotes", 0);
+		room.set("answered", false); 
+		room.save(null, {
+	  		success: function(gameScore) {
+	    		// Execute any logic that should take place after the object is saved.
+	    		alert('New object created with objectId: ' + room.id);
+	  		},
+	  		error: function(gameScore, error) {
+	    		// Execute any logic that should take place if the save fails.
+	    		// error is a Parse.Error with an error code and message.
+	    		alert('Failed to create new object, with error code: ' + error.message);
+	  		}
+		});
+
+		var TestObject = Parse.Object.extend("TestObject");
+		var testObject = new TestObject();
+		testObject.save({foo: "bar"}).then(function(object) {
+	  		alert("yay! it worked");
+		});
 	});
 });
+
 
 
