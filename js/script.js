443ba72508a373af80f72336848ebc2e20de2b59
albertliangcode <albertliangcode@gmail.com>
2015-02-21 14:59:23 -0800

    Front End Incomplete

--- js/script.js
+++ js/script.js
@@ -44,7 +44,7 @@ $(document).ready(function()
 		var TestObject = Parse.Object.extend("TestObject");
 		var testObject = new TestObject();
 		testObject.save({foo: "bar"}).then(function(object) {
-	  		alert("yay! it worked");
+	  		//alert("yay! it worked");
 		});
 	});
 });
