1ead5e5a8acc515ac6286d516b66fff152f0b2e0
albertliangcode <albertliangcode@gmail.com>
2015-02-21 12:20:34 -0800

    Add Post

--- js/script.js
+++ js/script.js
@@ -11,11 +11,11 @@ $(document).ready(function()
 		var room = new Classroom();
 		console.log("Classroom declared");
 		
-		// var Post = Parse.Object.Classroom.extend("Post");
-		// var post = new Post();
-		// post.set("upvotes", 0);
-		// post.set("downvotes", 0);
-		// post.set("answered", false); 
+		var Post = Parse.Object.extend("Post");
+		var post = new Post();
+		post.set("upvotes", 0);
+		post.set("downvotes", 0);
+		post.set("answered", false); 
 		// console.log("Post declared");
 
 		room.save(null, {
@@ -31,18 +31,18 @@ $(document).ready(function()
 	  		}
 		});
 
-		// post.save(null, {
-	 //  		success: function(Post) {
-	 //    		// Execute any logic that should take place after the object is saved.
-	 //    		alert('New object created with objectId: ' + post.id);
-	 //  		},
-	 //  		error: function(Post, error) {
-	 //    		// Execute any logic that should take place if the save fails.
-	 //    		// error is a Parse.Error with an error code and message.
-	 //    		alert('Failed to create new object, with error code: ' + error.message);
-	 //  			//console.log("Nope.");
-	 //  		}
-		// });
+		post.save(null, {
+	  		success: function(Post) {
+	    		// Execute any logic that should take place after the object is saved.
+	    		alert('New object created with objectId: ' + post.id);
+	  		},
+	  		error: function(Post, error) {
+	    		// Execute any logic that should take place if the save fails.
+	    		// error is a Parse.Error with an error code and message.
+	    		alert('Failed to create new object, with error code: ' + error.message);
+	  			//console.log("Nope.");
+	  		}
+		});
 
 		var TestObject = Parse.Object.extend("TestObject");
 		var testObject = new TestObject();
