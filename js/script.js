1d6f13111e84302a0a79a93937ce7688c4092484
albertliangcode <albertliangcode@gmail.com>
2015-02-21 21:25:27 -0800

    Make sure Classroom works

--- js/script.js
+++ js/script.js
@@ -27,6 +27,8 @@ $(document).ready(function(){
 					console.log(results[0].attributes.code);
 					console.log(results[0].id);
 					console.log("YAAASSS!!!!\n");
+					Classroom = search.get(results[0].id);
+					console.log(Classroom);
 
 				}
 			},
