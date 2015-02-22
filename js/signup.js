c3110e461709be67c497e3adb82944a17f1faf3f
xinlei <wang.xinlei15@gmail.com>
2015-02-21 19:59:25 -0800

    form for creating a new class

--- js/signup.js
+++ js/signup.js
@@ -1,22 +1,27 @@
-Parse.initialize("HnLswO6JpYmn4QrX2ClgADpA3HN2GFiVS1V95RP3", "IhuA6xPlWchDYpifcYQ39V18VAe9Dh44TgWBD2t1");
-var Classroom = Parse.Object.extend({
-	className: "Classroom"
-});
-var room = new Classroom();
-room.set("code","");
-room.set("school","");
-room.set("className","");
-room.set("numStudents",0);
-room.set("inSession",false);
+$(document).ready(function(){
+	$('#new-class').submit(function(){
+		Parse.initialize("HnLswO6JpYmn4QrX2ClgADpA3HN2GFiVS1V95RP3", "IhuA6xPlWchDYpifcYQ39V18VAe9Dh44TgWBD2t1");
+		
+		var Classroom = Parse.Object.extend("Classroom");
+		var room = new Classroom();
+		
+		room.set("code",$('#inputCode').val());
+		room.set("school", $('#inputSchool').val());
+		room.set("name", $('#inputName').val());
+		room.set("numStudents", $('#inputNumber').val());
+		room.set("inSession",$('#check_inSession').val());
 
-room.save(null, {
-	success: function(Classroom) {
-	    // Execute any logic that should take place after the object is saved.
-	    //alert('New object created with objectId: ' + room.id);
-	},
-	error: function(Classroom, error) {
-	    // Execute any logic that should take place if the save fails.
-	    // error is a Parse.Error with an error code and message.
-	    alert('Failed to create new object, with error code: ' + error.message);
-	}
-});
+		room.save(null, {
+			success: function(Classroom) {
+	    		// Execute any logic that should take place after the object is saved.
+	    		//alert('New object created with objectId: ' + room.id);
+			},
+			error: function(Classroom, error) {
+	    		// Execute any logic that should take place if the save fails.
+	    		// error is a Parse.Error with an error code and message.
+	    		alert('Failed to create new object, with error code: ' + error.message);
+			}
+		});
+		
+	});
+}); 
