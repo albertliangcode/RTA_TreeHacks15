58b6fd2e11940cc4e4174151d0601a2bb9ef6697
albertliangcode <albertliangcode@gmail.com>
2015-02-21 17:20:32 -0800

    Classroom not declared in Script fix attempt 2

--- js/signup.js
+++ js/signup.js
@@ -1,19 +1,20 @@
+Parse.initialize("HnLswO6JpYmn4QrX2ClgADpA3HN2GFiVS1V95RP3", "IhuA6xPlWchDYpifcYQ39V18VAe9Dh44TgWBD2t1");
 var Classroom = Parse.Object.extend({
-			className: "Classroom"
-		});
-		var room = new Classroom();
-		room.set("name","");
-		room.set("numStudents",0);
-		room.set("inSession",false);
+	className: "Classroom"
+});
+var room = new Classroom();
+room.set("name","");
+room.set("numStudents",0);
+room.set("inSession",false);
 
-		room.save(null, {
-	  		success: function(Classroom) {
-	    		// Execute any logic that should take place after the object is saved.
-	    		//alert('New object created with objectId: ' + room.id);
-	  		},
-	  		error: function(Classroom, error) {
-	    		// Execute any logic that should take place if the save fails.
-	    		// error is a Parse.Error with an error code and message.
-	    		alert('Failed to create new object, with error code: ' + error.message);
-	  		}
-		});
+room.save(null, {
+	success: function(Classroom) {
+	    // Execute any logic that should take place after the object is saved.
+	    //alert('New object created with objectId: ' + room.id);
+	},
+	error: function(Classroom, error) {
+	    // Execute any logic that should take place if the save fails.
+	    // error is a Parse.Error with an error code and message.
+	    alert('Failed to create new object, with error code: ' + error.message);
+	}
+});
